fe8eae2c670780d94098000ff0e6970b8b2e0f92
Anjali <holla2467@gmail.com>
2023-10-08 16:06:35 +0530

    ProfileView code logic is tested

--- server/server/api/urls.py
+++ server/server/api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
-from api.views import FormView, ResultView
+from api.views import FormView, ResultView, ProfileView
 
 from .views import index
 
@@ -10,6 +10,7 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path('',index, name="home"),
+    path('profile/', ProfileView.as_view(), name='profileview'),
     path('form/', FormView.as_view(), name='formview'),
     path('form/result/', ResultView.as_view(), name='resultview'),
     path('',include(router.urls))
