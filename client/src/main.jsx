d0d0716f567eceda20bb0ba0e523bf306de32a4f
singodiyashubham87 <singodiyashubham87@gmail.com>
2023-10-31 18:58:55 +0530

    refactor: remove unnecessary logs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,11 +9,6 @@ const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URL;
 
-console.log(domain)
-console.log(clientId)
-console.log(redirectUri)
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
