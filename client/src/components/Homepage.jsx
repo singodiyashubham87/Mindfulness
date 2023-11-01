1c271e436297b7f1b731da4214826b1bba30dfe6
singodiyashubham87 <singodiyashubham87@gmail.com>
2023-11-01 15:14:36 +0530

    feat: routing between different pages done

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,6 +1,7 @@
 import landingPageBanner from "../assets/images/landingPageBg.png";
 import logo from "../assets/images/logo.png";
 import avatar from "../assets/images/avatar.png";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
@@ -13,24 +14,28 @@ function Homepage() {
             className="absolute bottom-[0] w-full 2xl:bottom-[-10%] xxl:bottom-[-18%]"
           />
         </div>
-        <nav className="flex items-center px-[4rem] mt-[1rem] justify-between">
-          <img src={logo} className="w-[7rem]" alt="logo" />
-          <div className="userProfile w-[4rem] h-[4rem] rounded-[50%] border-2 border-black overflow-hidden cursor-pointer">
-            <img
-              src={avatar}
-              alt="avatarLogo"
-              className="w-full h-full object-cover cursor-pointer"
-            />
-          </div>
+        <nav className="flex items-center px-[2rem] mt-[1rem] justify-between lg:px-[4rem] lg:mt-[2rem] xl:pr-[8rem] 2xl:pr-[12rem]">
+          <img src={logo} className="vvsm:w-[6rem] lg:w-[8rem]" alt="logo" />
+          <Link to={"/track"}>
+            <div className="userProfile w-[4rem] h-[4rem] rounded-[50%] border-2 border-black overflow-hidden cursor-pointer">
+              <img
+                src={avatar}
+                alt="avatarLogo"
+                className="w-full h-full object-cover cursor-pointer"
+              />
+            </div>
+          </Link>
         </nav>
         <div className="landingPageText m-auto flex flex-col items-center justify-center w-[100%] absolute top-[50%] left-[50%] translate-x-[-50%]	translate-y-[-50%]">
           <h1 className="text-[5rem] leading-[1.2]">Mindfulness</h1>
           <h4 className="text-[#5F5A5A] text-[2rem] w-[90%] text-center mt-[0.5rem] md:w-[60%] mmd:w-[55%] lg:w-[45%] xl:w-[40%]">
             Assess your mental health status by answering some questions.
           </h4>
-          <button className="transition ease-in-out  text-[2rem] bg-[#D9D9D9] py-[0.3rem] px-[2rem] rounded-[2rem] mt-[0.8rem] hover:bg-black hover:text-[#D9D9D9]">
-            Start Test
-          </button>
+          <Link to={"/assessment"}>
+            <button className="transition ease-in-out  text-[2rem] bg-[#D9D9D9] py-[0.3rem] px-[2rem] rounded-[2rem] mt-[0.8rem] hover:bg-black hover:text-[#D9D9D9]">
+              Start Test
+            </button>
+          </Link>
         </div>
       </div>
     </>
--- client/src/components/ResultPage.jsx
+++ client/src/components/ResultPage.jsx
@@ -1,16 +1,29 @@
 import resultArt from "../assets/images/resultArt.png";
 import { useState } from "react";
+import logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 function ResultPage() {
   const [status, setStatus] = useState("Wohoo!");
   const [testResult, setTestResult] = useState("Little or no Stress!");
-  const [recommendation, setRecommendation] = useState("Stay joyful and be relaxed");
+  const [recommendation, setRecommendation] = useState(
+    "Stay joyful and be relaxed"
+  );
 
   return (
     <>
-      <div className="resultContainer bg-[#FFCE00] w-[100%] h-[100vh] relative font-primary flex justify-center items-center">
+      <div className="resultContainer bg-[#FFCE00] w-[100%] h-[100vh] relative font-primary flex justify-center items-center relative">
+        <Link to={"/"}>
+          <img
+            src={logo}
+            alt="logo"
+            className="absolute top-[2rem] left-[2rem] vvsm:w-[6rem] lg:w-[8rem] lg:top-[2rem] lg:left-[2rem]"
+          />
+        </Link>
         <div className="resultText w-[85%] h-[55%] md:h-[60%] md:w-[75%] flex flex-col justify-evenly items-center">
-          <h1 className="vvsm:text-[3rem] vsm:text-[3.5rem] gsm:text-[4rem] md:text-[5rem] leading-[1.2]">{status}</h1>
+          <h1 className="vvsm:text-[3rem] vsm:text-[3.5rem] gsm:text-[4rem] md:text-[5rem] leading-[1.2]">
+            {status}
+          </h1>
           <div className="testResult bg-[#FF7F1E] rounded-[2rem] vvsm:rounded-[1rem] px-[2rem] text-center">
             <h2 className="vvsm:text-[1.3rem] vsm:text-[1.5rem] msm:text-[1.7rem] gsm:text-[2rem] 2xl:text-[3rem] md:text-[2.5rem] lg:text-[2.8rem] text-white">
               Test Result: <span className="text-black">{testResult}</span>
@@ -18,11 +31,13 @@ function ResultPage() {
           </div>
           <div className="recommendation bg-[#E03A3A]  rounded-[2rem] vvsm:rounded-[1rem] px-[2rem] text-center">
             <h2 className="vvsm:text-[1.3rem] vsm:text-[1.5rem] msm:text-[1.7rem] gsm:text-[2rem] 2xl:text-[3rem] md:text-[2.5rem] lg:text-[2.8rem] text-white">
-            Recommendation: <span className="text-[#FAC05E]">{recommendation}</span>
+              Recommendation:{" "}
+              <span className="text-[#FAC05E]">{recommendation}</span>
             </h2>
           </div>
-          <button className="transition ease-in-out vvsm:text-[1.3rem] vsm:text-[1.5rem] lg:text-[1.5rem] gsm:text-[1.8rem]  md:text-[2rem] lg:text-[2.5rem] bg-[#D9D9D9] py-[0.3rem] px-[2rem] rounded-[2rem] mt-[0.8rem] hover:bg-black hover:text-[#D9D9D9]">Save Progress</button>
-
+          <button className="transition ease-in-out vvsm:text-[1.3rem] vsm:text-[1.5rem] lg:text-[1.5rem] gsm:text-[1.8rem]  md:text-[2rem] lg:text-[2.5rem] bg-[#D9D9D9] py-[0.3rem] px-[2rem] rounded-[2rem] mt-[0.8rem] hover:bg-black hover:text-[#D9D9D9]">
+            Save Progress
+          </button>
         </div>
         <img
           src={resultArt}
