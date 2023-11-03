3ddc40f6a9707bb907269401dfc102950a4d20e7
Anjali <holla2467@gmail.com>
2023-11-03 08:23:05 +0530

    added cors header

--- server/server/server/settings.py
+++ server/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2psv)g=epunil*df#2m0d^&r5hq+v5jg6$eww0_pff%7vp)&uu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
