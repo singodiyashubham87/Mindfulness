e44e241e2c16434c1555458bfa32774e8ee581ef
singodiyashubham87 <singodiyashubham87@gmail.com>
2023-11-22 11:56:01 +0530

    add: authentication button in result page and save result on authentication only

--- client/src/pages/ResultPage.jsx
+++ client/src/pages/ResultPage.jsx
@@ -5,6 +5,7 @@ import { Link, useLocation } from "react-router-dom";
 import Loader from "../components/Loader";
 import Modal from "../components/Modal";
 import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function ResultPage() {
   // Get the current location
@@ -12,30 +13,40 @@ function ResultPage() {
   // Extract the assessment score from the location state
   const assessmentScore = location.state?.assessmentScore.score;
 
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
   // States to manage various aspects of the result
   const [status, setStatus] = useState("Not Working!");
   const [testResult, setTestResult] = useState("Kam hi nahi kar raha!");
   const [recommendation, setRecommendation] = useState("Kuch mat kar bhai");
   const [loader, setLoader] = useState(false);
+  const [alertError, setAlertError] = useState("Tracking Result Saved!"); //alert message in modal component
   const [showModal, setshowModal] = useState(false); //toggle popup modal
 
   // Show & Hide Model
   const openModal = () => setshowModal(true);
   const closeModal = () => setshowModal(false);
 
-    // Show & Hide Loader
-    const showLoader = () => setLoader(true);
-    const hideLoader = () => setLoader(false);
-  
+  // Show & Hide Loader
+  const showLoader = () => setLoader(true);
+  const hideLoader = () => setLoader(false);
 
   // Handle click on Save Progress Button
   const handleSaveProgress = async () => {
-    showLoader();
-    const data = {"user":"examplee@gmail.com", "score": 33};
-    const res = await axios.post("http://127.0.0.1:8000/api/saveresult/",data)
-    hideLoader();
-    openModal();
-  }
+    if (!isAuthenticated) {
+      setAlertError("Login to save result.");
+      openModal();
+    } else {
+      showLoader();
+      const data = { user: "examplee@gmail.com", score: 33 };
+      const res = await axios.post(
+        "http://127.0.0.1:8000/api/saveresult/",
+        data
+      );
+      hideLoader();
+      openModal();
+    }
+  };
 
   // Use useEffect to modify status based on assessmentScore changes
   useEffect(() => {
@@ -55,20 +66,50 @@ function ResultPage() {
     }
   }, []); // Empty dependency array to run the effect only once
 
+  // Handle user login
+  const handleLogin = () => {
+    loginWithRedirect({
+      authorizationParams: { redirect_uri: window.location.href },
+    });
+  };
+
+  // Handle user logout
+  const handleLogout = () => {
+    // localStorage.clear(); // Clear local storage before logging out
+    logout({ logoutParams: { returnTo: window.location.href } });
+  };
+
   return (
     <>
       {showModal && (
-        <Modal alert="Alert!" alertError="Tracking Result Saved!" closeModal={closeModal} />
+        <Modal alert="Alert!" alertError={alertError} closeModal={closeModal} />
       )}
-      {loader && <Loader/>}
+      {loader && <Loader />}
       <div className="resultContainer bg-[#FFCE00] w-[100%] h-[100vh] relative font-primary flex justify-center items-center relative">
-        <Link to={"/"}>
-          <img
-            src={logo}
-            alt="logo"
-            className="absolute top-[2rem] left-[2rem] vvsm:w-[5rem] msm:w-[6rem] lg:w-[8rem] lg:top-[2rem] lg:left-[2rem]"
-          />
-        </Link>
+        <div className="w-[95dvw] flex items-center px-[0.1rem] sm:px-[2rem] mt-[1rem] justify-between md:w-[99dvw] lg:px-[3rem] 2xl:px-[4rem] lg:mt-[1rem] absolute top-[1rem]">
+          <Link to={"/"}>
+            <img
+              src={logo}
+              className="vvsm:w-[5rem] md:w-[6rem] lg:w-[8rem] cursor-pointer"
+              alt="logo"
+            />
+          </Link>
+          {isAuthenticated ? (
+            <button
+              className="bg-[#FF8020] text-black text-[1.3rem] sm:text-[1.5rem] px-[1rem] sm:px-[2rem] hover:bg-white hover:text-black border-2 border-black rounded-[0.625rem]"
+              onClick={handleLogout}
+            >
+              Log Out
+            </button>
+          ) : (
+            <button
+              className="bg-[#FF8020] text-black text-[1.2rem] vsm:text-[1.5rem] px-[1rem] vsm:px-[1.5rem] sm:px-[3rem] hover:bg-white hover:text-black border-2 border-black rounded-[0.625rem] z-[1]"
+              onClick={handleLogin}
+            >
+              Log In
+            </button>
+          )}
+        </div>
 
         <div className="resultText w-[85%] h-[55%] md:h-[60%] md:w-[75%] flex flex-col justify-evenly items-center">
           <h1 className="vvsm:text-[3rem] vsm:text-[3.5rem] gsm:text-[4rem] md:text-[5rem] leading-[1.2]">
@@ -85,7 +126,10 @@ function ResultPage() {
               <span className="text-[#FAC05E]">{recommendation}</span>
             </h2>
           </div>
-          <button className="transition ease-in-out vvsm:text-[1.3rem] vsm:text-[1.5rem] lg:text-[1.5rem] gsm:text-[1.8rem]  md:text-[2rem] lg:text-[2.5rem] bg-[#D9D9D9] py-[0.3rem] px-[2rem] rounded-[2rem] mt-[0.8rem] hover:bg-black hover:text-[#D9D9D9]" onClick={handleSaveProgress}>
+          <button
+            className="transition ease-in-out vvsm:text-[1.3rem] vsm:text-[1.5rem] lg:text-[1.5rem] gsm:text-[1.8rem]  md:text-[2rem] lg:text-[2.5rem] bg-[#D9D9D9] py-[0.3rem] px-[2rem] rounded-[2rem] mt-[0.8rem] hover:bg-black hover:text-[#D9D9D9]"
+            onClick={handleSaveProgress}
+          >
             Save Progress
           </button>
         </div>
