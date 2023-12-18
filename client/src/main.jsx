12216f4fc4cfb995532708d9911de23f2b852d4d
Shubham Singodiya <singodiyashubham87@gmail.com>
2023-12-19 01:59:01 +0530

    chore: cacheLocation updated from cookie to memory

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain={domain}
       clientId={clientId}
       useRefreshTokens={true}
-      cacheLocation="cookie"
+      cacheLocation="memory"
       authorizationParams={{
         redirect_uri: redirectUri
       }}
